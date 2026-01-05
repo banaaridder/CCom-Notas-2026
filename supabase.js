195be9e21a593a1421b0f71d17f678294a5f28ba
Davi Costa <122738087+banaaridder@users.noreply.github.com>
2026-01-05 15:21:27 -0300

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,11 +1,10 @@
+// supabase.js
 const SUPABASE_URL = "https://rqsokwtewfmvdnsyddrn.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_TlJfKaHL_skoJbrD6kfBzQ_gIcnGTx_";
 
-const supabase = supabase.createClient(_supabaseUrl, _supabaseKey)
+// A variável precisa se chamar apenas 'supabase' para bater com o campo.js
+// Usamos window.supabase.createClient pois a biblioteca do CDN coloca o comando dentro do objeto window
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-window.supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
-
-console.log("Supabase OK:", window.supabaseClient);
+// Log para confirmar no console se carregou
+console.log("Supabase inicializado com sucesso!");
